cb15970ec3b7d242173da4f3be1402c0e1f9107d
ieunune <une@kakao.com>
2022-10-25 12:36:46 +0900

    [chroe] reactivity 복습

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<!-- 파일 이름 명명 규칙은 PascalCase, 두 단어 이상 결합할 것 -->
+<template>
+    <header>
+        <h1>{{ propsdata }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata']
+}
+</script>
+
+<style>
+
+</style>
